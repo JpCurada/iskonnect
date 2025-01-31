6997f62ed90543d69254102069f42878fb0e346c
JpCurada <j.curada02@gmail.com>
2025-01-31 19:56:06 +0800

    refactor:cleaned inheritance concept

--- src/main/java/com/iskonnect/controllers/AdminViewController.java
+++ src/main/java/com/iskonnect/controllers/AdminViewController.java
@@ -1,72 +1,29 @@
 // Path: src/main/java/com/iskonnect/controllers/AdminViewController.java
-
 package com.iskonnect.controllers;
 
-import com.iskonnect.application.Main;
-import com.iskonnect.utils.UserSession;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import java.io.IOException;
 
-public class AdminViewController {
-    @FXML private Text adminNameText;
-    @FXML private Text adminIdText;
-    @FXML private VBox contentArea;
-
+public class AdminViewController extends BaseViewController {
+    
     @FXML
+    @Override
     public void initialize() {
-        initializeAdminInfo();
-        loadPage("dashboard"); // Load default page
-    }
-
-    private void initializeAdminInfo() {
-        UserSession session = UserSession.getInstance();
-        if (session != null) {
-            adminNameText.setText(session.getFullName());
-            adminIdText.setText(session.getUserId());
-        }
+        super.initialize();
+        loadPage("admin", "dashboard");
     }
 
     @FXML
-    private void handleDashboard() {
-        loadPage("dashboard");
+    public void handleDashboard() {
+        loadPage("admin", "dashboard");
     }
 
     @FXML
-    private void handleUsers() {
-        loadPage("users");
+    public void handleUsers() {
+        loadPage("admin", "users");
     }
 
     @FXML
-    private void handleMaterials() {
-        loadPage("materials");
-    }
-
-    @FXML
-    private void handleReports() {
-        loadPage("reports");
-    }
-
-    @FXML
-    private void handleLogout() {
-        UserSession.getInstance().endSession();
-        try {
-            Main.setLoginRoot("auth/login");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void loadPage(String page) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin/" + page + ".fxml"));
-            VBox pageContent = loader.load();
-            contentArea.getChildren().setAll(pageContent);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Error loading " + page + ": " + e.getMessage());
-        }
+    public void handleReports() {
+        loadPage("admin", "reports");
     }
 }
